32bd7049fe11239a8ac2d16dab72e64593f3a98b
Vai Mor <thevaimor@icloud.com>
2022-10-12 06:07:10 +0000

    Refactor some default code

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-var-requires */
 /** @type {import('tailwindcss').Config} */
 
 const colors = require('tailwindcss/colors');
